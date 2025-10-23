0ec25c1f2f4aba35c0bf41bc959a82a1ee0e6d47
Thomas Rueckstiess <me@tomr.au>
2025-10-23 14:34:44 +1100

    Re-export SampleEstimator from mdbrtools

--- origami/inference/__init__.py
+++ origami/inference/__init__.py
@@ -5,3 +5,16 @@ from .autocomplete import AutoCompleter
 from .sampler import Sampler
 from .mc_estimator import MCEstimator
 from .rejection_estimator import RejectionEstimator
+from mdbrtools.estimator import SampleEstimator
+
+
+__all__ = [
+    'Predictor',
+    'Embedder',
+    'Metrics',
+    'AutoCompleter',
+    'Sampler',
+    'MCEstimator',
+    'RejectionEstimator',
+    'SampleEstimator',  # Re-exported from mdbrtools
+]
